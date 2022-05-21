8a80d93a8b558a893867d854f199d78b2ce655ac
Ethan Genser <ethanp1033@gmail.com>
2022-05-21 13:35:28 -0500

    initial commit

---
+++ maze.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    var canvas = document.getElementById("maze-canvas");
+    var canvasCtx = canvas.getContext("2d");
+    canvasCtx.canvas.style.width = "100%";
+    canvasCtx.canvas.style.height = "100%";
+})
+
